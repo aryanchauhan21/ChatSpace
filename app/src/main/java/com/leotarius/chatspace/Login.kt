5af3366680b6894206f92e68a8c9f8afeef66325
aryanchauhan21 <sansarya2121@gmail.com>
2020-11-21 18:45:27 +0530

    added registration layout

--- app/src/main/java/com/leotarius/chatspace/Login.kt
+++ app/src/main/java/com/leotarius/chatspace/Login.kt
@@ -1,13 +1,17 @@
 package com.leotarius.chatspace
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
 
-
-
+        register.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
