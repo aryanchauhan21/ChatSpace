9b819892e710ab5017da2eb0deff2eeca6aaac85
aryanchauhan21 <sansarya2121@gmail.com>
2020-11-23 21:18:50 +0530

    more changes and updates

--- app/src/main/java/com/leotarius/chatspace/MainActivity.kt
+++ app/src/main/java/com/leotarius/chatspace/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.leotarius.chatspace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -13,6 +15,7 @@ import com.google.firebase.database.ValueEventListener
 import com.leotarius.chatspace.models.Message
 import com.leotarius.chatspace.models.User
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_toolbar2.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,17 +24,22 @@ class MainActivity : AppCompatActivity() {
         var dbUser:User? = null
     }
 
+    private val TAG = "MainActivity"
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val user = FirebaseAuth.getInstance().currentUser
-        val dbreference = user?.uid?.let { FirebaseDatabase.getInstance().reference.child(it) }
+        val dbreference = FirebaseDatabase.getInstance().reference.child("users").child(user?.uid!!)
 
-        dbreference?.addValueEventListener(object: ValueEventListener {
+        dbreference?.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 dbUser = snapshot.getValue(User::class.java)
+                Log.d(TAG, "onDataChange: ${dbUser.toString()}")
+                if (dbUser != null) Log.d(TAG, "onDataChange: not null")
                 dbreference.removeEventListener(this)
+                Log.d(TAG, "onDataChange: got user from database")
             }
 
             override fun onCancelled(error: DatabaseError) {}
@@ -40,7 +48,9 @@ class MainActivity : AppCompatActivity() {
         val reference = FirebaseDatabase.getInstance().reference
 
         send.setOnClickListener {
+            Log.d(TAG, "onCreate: click")
             if(dbUser!=null){
+                Log.d(TAG, "onCreate: clickedin")
                 val text = message.text.toString()
                 val chatMessage = Message(dbUser?.uid, text, dbUser?.name)
                 if(text.isNotEmpty()){
@@ -50,16 +60,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
+
         val layoutManager = LinearLayoutManager(this)
         val mList = ArrayList<Message>()
-        mList.add(Message(user?.uid,"hey man", "name"))
-        mList.add(Message("4","hey man2", "name"))
-        mList.add(Message("2","hey man3", "name"))
-        mList.add(Message(user?.uid,"hey man4", "name"))
-        mList.add(Message(user?.uid,"hey man4", "name"))
-        mList.add(Message(user?.uid,"hey man4", "name"))
-        mList.add(Message(user?.uid,"hey man4", "name"))
-        mList.add(Message(user?.uid,"hey man4", "name"))
+        mList.add(Message("1","Hey man", "Aryan Bharadwaj"))
+        mList.add(Message(user?.uid,"Hey mate", "Aryan Chauhan"))
+        mList.add(Message("1","How you doing?", "Aryan Bharadwaj"))
+        mList.add(Message(user?.uid,"I'm good buddy.", "Aryan Chauhan"))
+        mList.add(Message(user?.uid,"You say", "Aryan Chauhan"))
         viewAdapter = ChatAdapter(this, mList)
 
         val recyclerView:RecyclerView = findViewById(R.id.recycler_view)
